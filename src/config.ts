ea5864b7f55973cf4f19b2d12b8c9259edeb4b96
usbcombine <usbcombine.dev@gmail.com>
2020-09-15 22:35:50 +0200

    k

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = "https://api.github.com/";
 export const GRAPHQL_ENDPOINT = "https://api.github.com/graphql";
-export const GITHUB_PA_TOKEN = "26d1155eda34204d1b868f6f80acd48858173419";
+export const GITHUB_PA_TOKEN = "3d0e4de12a127709431ad46706aa1d3a18f50336";
